494a9b5c8f8e759ab9ea71b92f62aa69519e86cf
stablecoinchf <78139955+stablecoinchf@users.noreply.github.com>
2021-03-09 14:44:04 +0100

    Update BondCampaign.sol

--- BondCampaign.sol
+++ BondCampaign.sol
@@ -10,34 +10,95 @@ contract BondCampaign  {
     
     uint targetPrice  = uint(100).mul(one8);
     
-    uint public priceDiscount = 70;
+    uint  minPrice = uint(98).mul(one8);
+    
+    uint startTime;
+    
+    uint duration = 300;
+    
+    uint private priceDiscount = 70;
     
     uint public amount;
-
+    
     uint public price;
+
     
-    
-    constructor (uint  price_, uint totalsupply_) public {
-        _update(price_,totalsupply_);
+    constructor (uint  price_, uint totalsupply_, uint minPrice_) public {
+        _update(price_,totalsupply_, minPrice_);
     }
     
-     function decreaseAmount(uint amount_) public {
+    function restTime()  public view returns (uint) {
+        uint resttime = now - startTime;
+        if (resttime >= duration) {
+            resttime = 0;
+        } else {
+            resttime = duration - resttime;
+        }
+        return resttime;
+    }
+    
+    function getCoinBuyPrice(uint _price) public view returns (uint) {
+        uint buyPrice = 0;
+        if (_price< minPrice) {
+            buyPrice = _price.mul(102).div(100);
+        }
+        if (buyPrice > minPrice) {
+            buyPrice = minPrice;
+        }
+        return buyPrice;
+    }
+    
+    function getAmountOfCoins() public view returns (uint) {
+        return amount.mul(getBondPrice()).div(one8).div(100);
+    }
+    
+     function getBondPrice() public view returns (uint) {
+        uint bondPrice = 0;
+        if (price < minPrice) {
+            bondPrice = price.mul(priceDiscount).div(100);
+        }
+        return bondPrice;
+    } 
+    
+    
+    function bondSold(uint amount_) public {
         amount = amount.sub(amount_);
     }
     
-     function update(uint  price_, uint totalsupply_) public {
-        _update(price_,totalsupply_);
+    function coinBought(uint amount_) public {
+        uint bondreduction = 0;
+        if (getBondPrice()>0) {
+             bondreduction = amount_.mul(one8).div(getBondPrice()).mul(100);    
+        }
+        
+        amount = amount.sub(bondreduction);
+    }
+    
+     function update(uint  price_, uint totalsupply_, uint minPrice_) public {
+         
+         _update(price_,totalsupply_,minPrice_); 
+         
      }
      
-     function _update(uint  price_, uint totalsupply_) private {
-         if (targetPrice>price_ && price_> 0 && totalsupply_> 0) {
-            amount = targetPrice.sub(price_).mul(totalsupply_).div(one8).div(priceDiscount);
-            price = price_;
-        } else {
+     function _update(uint  price_, uint totalsupply_, uint minPrice_) private {
+         minPrice = minPrice_;
+         
+         if (!(restTime() > 0)) {
+            price = price_;    
+         }
+         
+         
+         if ((price_ > minPrice) || totalsupply_==0 ) {
             price = 0;
             amount = 0;
-        }
+         } 
+         
+         if (restTime()==0 && targetPrice>price_ && price_> 0 && totalsupply_> 0) {
+            amount = targetPrice.sub(price).mul(totalsupply_).div(one8).div(priceDiscount);
+            price = price_;
+            startTime = now;
+         }    
+         
      }
-     
      
 }
